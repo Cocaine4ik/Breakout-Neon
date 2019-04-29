8fdd3bb208711970459825d58bb3bee71d3f888c
Cocaine <kokotkin.alexander@gmail.com>
2019-04-29 10:58:10 +0300

    Update ConfigurationData.cs
    add comments, add balleLifeTime field

--- Assets/Scripts/Config/ConfigurationData.cs
+++ Assets/Scripts/Config/ConfigurationData.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System;
 
 /// A container for the configuration data
-/// 
 public class ConfigurationData
 {
     #region Fields
@@ -15,45 +14,48 @@ public class ConfigurationData
     // configuration data
     static float paddleMoveUnitsPerSecond = 10;
     static float ballImpulseForce = 400;
+    static float ballLifetime = 10;
 
     #endregion
 
     #region Properties
 
-    /// Gets the paddle move units per second
+    // gets the paddle move units per second
     public float PaddleMoveUnitsPerSecond
     {
         get { return paddleMoveUnitsPerSecond; }
     }
 
-    /// <summary>
-    /// Gets the impulse force to apply to move the ball
-    /// </summary>
-    /// <value>impulse force</value>
+    // gets the ball impulse force
     public float BallImpulseForce
     {
         get { return ballImpulseForce; }    
+    }
+
+    // gets the ball lifetime in seconds
+    public float BallLifetime {
+        get { return ballLifetime; }
     }
 
     #endregion
 
     #region Constructor
 
-    /// Constructor
-    /// Reads configuration data from a file. If the file
-    /// read fails, the object contains default values for
-    /// the configuration data
     public ConfigurationData()
     {
         StreamReader input = null;
 
         try {
-
+            // open and read ConfigurationData.csv file from Streaming assets folder
             input = File.OpenText(Path.Combine(Application.streamingAssetsPath, ConfigurationDataFileName));
+
+            // write fieled names to a variable
             string names = input.ReadLine();
 
+            // write field values to a variable
             string values = input.ReadLine();
-            Debug.Log("SetConfigurationDataFields..." + values);
+
+            // use values from csv to set values in game
             SetConfigurationDataFields(values);
 
         }
@@ -61,7 +63,9 @@ public class ConfigurationData
             
         }
         finally {
+
             if (input != null) {
+
                 input.Close();
             }
         }
@@ -69,12 +73,14 @@ public class ConfigurationData
     }
 
     #endregion
-
+    // parse values and set ConfigurationData fields
     void SetConfigurationDataFields (string csvValues) {
+
         string[] values = csvValues.Split(',');
 
         paddleMoveUnitsPerSecond = float.Parse(values[0]);
         ballImpulseForce = float.Parse(values[1]);
+        ballLifetime = float.Parse(values[2]);
 
     }
 }
