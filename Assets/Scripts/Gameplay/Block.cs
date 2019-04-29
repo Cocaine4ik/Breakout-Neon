094e4273759535ae4b34e9d8903f9d1b415be282
Cocaine <kokotkin.alexander@gmail.com>
2019-04-29 11:00:31 +0300

    Update Block.cs
    delte spaces

--- Assets/Scripts/Gameplay/Block.cs
+++ Assets/Scripts/Gameplay/Block.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
-
-	// Use this for initialization
-	void Start () {
-
-    }
-
     private void OnCollisionEnter2D(Collision2D coll) {
 
         if(coll.gameObject.CompareTag("Ball")) {
+
             Destroy(gameObject);
             
         }
