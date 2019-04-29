7637f89dcc5e7960562f0b0562c1a495247af647
Cocaine <kokotkin.alexander@gmail.com>
2019-04-29 10:59:46 +0300

    Update Ball.cs
    Add two timer for death and start moving. Transfer Addfororce() to Update() method.

--- Assets/Scripts/Gameplay/Ball.cs
+++ Assets/Scripts/Gameplay/Ball.cs
@@ -5,25 +5,49 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
     private Rigidbody2D rb;
+
+    private Timer deathTimer;
+    private Timer startMoveTimer;
+
     private float angle = 270 * Mathf.Deg2Rad;
 
-    // Use this for initialization
     void Start () {
 
         rb = GetComponent<Rigidbody2D>();
-         
-        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-        Debug.Log(direction);
 
-        rb.AddForce(direction * ConfigurationUtils.BallImpulseForce * Time.deltaTime, ForceMode2D.Impulse);
+        // run and configure deathTimer
+        deathTimer = gameObject.AddComponent<Timer>();
+        deathTimer.Duration = ConfigurationUtils.BallLifetime;
+        deathTimer.Run();
 
+        //run and configure startMoveTimer
+        startMoveTimer = gameObject.AddComponent<Timer>();
+        startMoveTimer.Duration = 1f;
+        startMoveTimer.Run();
 
     }
 
+    // set ball direction and sped after collision
     public void SetDirection(Vector2 ballDirection) {
 
         rb.velocity = ballDirection * ConfigurationUtils.BallImpulseForce * Time.deltaTime;
 
+    }
+    private void Update() {
+        // if startMoveTimer is finished add force to ball
+        if (startMoveTimer.Finished) {
+
+            Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+            rb.AddForce(direction * ConfigurationUtils.BallImpulseForce * Time.deltaTime, ForceMode2D.Impulse);
+            startMoveTimer.Stop();
+        }
+
+        // if deathTimer is finished spwn new ball and destroy previous
+        if (deathTimer.Finished) {         
+
+            Camera.main.GetComponent<BallSpawner>().SpawnBall();
+            Destroy(gameObject);
+        }
     }
 }
 
