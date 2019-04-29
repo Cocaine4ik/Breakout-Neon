54fe095b1489467bd5810d618ffcd90610297916
Cocaine <kokotkin.alexander@gmail.com>
2019-04-29 11:00:17 +0300

    Create BallSpawner.cs
    Create BallSpawner with SpawnBall() method

---
+++ Assets/Scripts/Gameplay/BallSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    [SerializeField] GameObject ballPrefab;
+
+    // spawn ball
+    public void SpawnBall() {
+        Instantiate(ballPrefab);
+    }
+}
