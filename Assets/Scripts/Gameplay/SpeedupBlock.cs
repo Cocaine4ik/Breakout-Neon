6b15508f121e8df366f0e004014827a3c2c882ab
Cocaine <kokotkin.alexander@gmail.com>
2019-05-01 15:01:05 +0300

    Create SpeedupBlock.cs

---
+++ Assets/Scripts/Gameplay/SpeedupBlock.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedupBlock : PickupBlock {
+
+    protected override void Start() {
+
+        base.Start();
+        color = "Green";
+        pickupEffect = PickupEffect.Speedup.ToString();
+    }
+}
