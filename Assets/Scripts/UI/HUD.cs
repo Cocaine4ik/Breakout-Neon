f45978f7d7f6556fc710be9b3bedbb1a4c49cc06
Cocaine <kokotkin.alexander@gmail.com>
2019-05-01 15:01:20 +0300

    Create HUD.cs

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    private static GameObject scoresText;
+    private static GameObject ballsText;
+
+    private static int scores;
+    private static int balls;
+
+    void Start() {
+
+        balls = ConfigurationUtils.BallsPerGame;
+
+        scoresText = GameObject.FindGameObjectWithTag("Score");
+        ballsText = GameObject.FindGameObjectWithTag("Balls");
+        ballsText.GetComponent<Text>().text = "Balls: " + balls;
+    }
+    // add points to scroe value
+    public static void AddScore(int points) {
+
+        scores += points;
+        scoresText.GetComponent<Text>().text = "Score: " + scores.ToString();
+
+    }
+    // remove ball from balls per game
+    public static void  RemoveBall() {
+        balls = balls - 1;
+        ballsText.GetComponent<Text>().text = "Balls: " + balls;
+    }
+}
