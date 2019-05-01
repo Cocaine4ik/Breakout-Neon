1747cbe899ff1769c6ac5b698a63a67d0a8066a8
Cocaine <kokotkin.alexander@gmail.com>
2019-05-01 15:00:52 +0300

    Create PickupBlock.cs

---
+++ Assets/Scripts/Gameplay/PickupBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupBlock : StandardBlock {
+
+    protected string pickupEffect;
+
+    protected override void Start() {
+
+        points = ConfigurationUtils.PickupBlockPoints;
+                        
+    }
+}
