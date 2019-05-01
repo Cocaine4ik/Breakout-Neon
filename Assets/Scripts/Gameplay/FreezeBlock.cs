8d4ee677c3bbdec6a4d90db1864d08e4b68ff430
Cocaine <kokotkin.alexander@gmail.com>
2019-05-01 15:00:38 +0300

    Create FreezeBlock.cs

---
+++ Assets/Scripts/Gameplay/FreezeBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeBlock : PickupBlock {
+
+    protected override void Start() {
+
+        base.Start();
+        color = "Blue";
+        pickupEffect = PickupEffect.Freezer.ToString();
+
+    }
+}
