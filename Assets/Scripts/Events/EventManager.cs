e2e64dc3d50a548e4063f6f68c4d4957f75898c4
Cocaine <kokotkin.alexander@gmail.com>
2019-05-19 12:58:29 +0300

    Update EventManager.cs
    Full rewrite event anager using generics and diitionary

--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -3,6 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public static class EventManager{
+[System.Serializable]
+public class Event : UnityEvent<System.Object> { }
 
+public class EventManager : MonoBehaviour {
+
+    private Dictionary <string, Event> eventDictionary;
+
+    public static EventManager Instance;
+
+    private void Awake() {
+        if(Instance == null) {
+
+            Instance = this;
+            eventDictionary = new Dictionary<string, Event>();
+        }
+        else if (Instance != this) {
+
+            Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public static void StartListening(string eventName, UnityAction <System.Object> listener) {
+
+        Event thisEvent = null;
+        if(Instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+
+            thisEvent.AddListener(listener);
+        }
+        else {
+            thisEvent = new Event();
+            thisEvent.AddListener(listener);
+            Instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction <System.Object> listener) {
+
+        if (Instance = null) return;
+        Event thisEvent = null;
+
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName, System.Object arg = null) {
+
+        Event thisEvent = null;
+
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+
+            thisEvent.Invoke(arg);
+        }
+    }
 }
