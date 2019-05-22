3dba1b51f1cb37871d7f4c36a3625c3193bc8226
Cocaine4ik <kokotkin.alexander@gmail.com>
2019-05-22 10:47:46 +0300

    Update FreezeBlock.cs
    add freeze effect event invoker

--- Assets/Scripts/Gameplay/FreezeBlock.cs
+++ Assets/Scripts/Gameplay/FreezeBlock.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class FreezeBlock : PickupBlock {
 
-    private float freezeEffectDuarion;
+    private float freezeEffectDuration;
     FreezerEffectActivated freezerEffectActivated = new FreezerEffectActivated();
 
+    
     protected override void Start() {
 
         base.Start();
 
+
         color = "Blue";
 
         effect = PickupEffect.Freezer;
+        freezeEffectDuration = ConfigurationUtils.FreezerEffectDuarion;
     }
 
-    protected override void OnDestroy() {
+    protected override void OnCollisionEnter2D(Collision2D collision) {
 
-        base.OnDestroy();
-        EventManager.TriggerEvent("FreezeEffectActivated", freezeEffectDuarion);
+        base.OnCollisionEnter2D(collision);
+
+        EventManager.TriggerEvent("FreezeEffectActivated", freezeEffectDuration);
     }
 }
