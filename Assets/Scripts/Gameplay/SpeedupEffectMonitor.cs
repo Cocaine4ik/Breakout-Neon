a990d79ef81c0255b7d3f63b04f89ab6d2013c32
Cocaine <kokotkin.alexander@gmail.com>
2019-05-26 14:39:22 +0300

    Update SpeedupEffectMonitor.cs

--- Assets/Scripts/Gameplay/SpeedupEffectMonitor.cs
+++ Assets/Scripts/Gameplay/SpeedupEffectMonitor.cs
@@ -2,15 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedupEffectMonitor : MonoBehaviour {
+public class SpeedUpEffectMonitor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region Fields
+
+    Timer speedUpTimer;
+    private bool speedUp;
+
+    #endregion
+
+    #region Properties
+
+    public bool SpeedUp {
+        get { return speedUp; }
+    }
+    #endregion
+
+    #region Methods
+
+    private void Start() {
+
+        speedUpTimer = gameObject.AddComponent<Timer>();
+        EventManager.StartListening("SpeedUpEffectActivated", OnSpeedUp);
+    }
+
+    private void Update() {
+        if(speedUpTimer.Finished) {
+
+            speedUp = false;
+        }
+        Debug.Log(speedUp);
+    }
+    private void OnDestroy() {
+        EventManager.StopListening("SpeedUpEffectActivated", OnSpeedUp);
+    }
+
+    private void OnSpeedUp(object arg0, object arg1) {
+
+        if(!speedUpTimer.Running) {
+
+            speedUpTimer.Duration = (float)arg0;
+            speedUpTimer.Run();
+            speedUp = true;
+        }
+        else {
+
+            speedUpTimer.Duration = speedUpTimer.TimeLeft + (float)arg0;
+            speedUpTimer.Run();
+        }
+    }
+
+    #endregion
+
 }
