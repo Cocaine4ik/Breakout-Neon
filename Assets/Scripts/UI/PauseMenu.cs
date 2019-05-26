9bc4cae960327f474b9a14e88dc48b6a61842376
Cocaine <kokotkin.alexander@gmail.com>
2019-05-26 14:40:01 +0300

    Create PauseMenu.cs

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    #region Methods
+
+    void Start () {
+        // stop the game
+        Time.timeScale = 0;
+	}
+
+	// resume game and destroy pause menu
+    public void OnResumeButton() {
+
+        Time.timeScale = 1;
+        Destroy(gameObject);
+    }
+
+    // resume game, destroy pause menu and go to main menu
+    public void OnQuitButton() {
+
+        Time.timeScale = 1;
+        Destroy(gameObject);
+        MenuManager.GoToMenu(MenuName.Main);
+    }
+    #endregion
+
+
+}
