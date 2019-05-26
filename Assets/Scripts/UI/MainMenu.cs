92aaa0f7833367131a8213444bc8653526382fb7
Cocaine <kokotkin.alexander@gmail.com>
2019-05-26 14:39:38 +0300

    Create MainMenu.cs

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    #region Methods
+
+    // start new game
+    public void OnStartButton() {
+
+        SceneManager.LoadScene("Scene0");
+    }
+
+    // quit game
+    public void OnQuitButton() {
+
+        Application.Quit();
+    }
+    #endregion
+
+}
