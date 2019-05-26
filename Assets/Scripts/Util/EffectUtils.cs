6e577a1698093f5476f0ccf18f0a7f1f4cec7c6a
Cocaine <kokotkin.alexander@gmail.com>
2019-05-26 14:40:11 +0300

    Create EffectUtils.cs

---
+++ Assets/Scripts/Util/EffectUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectUtils{
+
+    #region Fields
+
+    static SpeedUpEffectMonitor speedUpEffectMonitor;
+
+    #endregion
+
+    #region Properties
+
+    public static bool SpeedUp {
+        get { return speedUpEffectMonitor.SpeedUp; }
+    }
+    #endregion
+
+    #region Methods
+
+    public static void Initialize() {
+        speedUpEffectMonitor = Camera.main.GetComponent<SpeedUpEffectMonitor>();
+    }
+
+    #endregion
+}
